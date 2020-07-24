8a93612e817ceb09ba916cec5d4d796cef6f51a8
mc0239 <martin.cebular@gmail.com>
2020-07-24 16:30:06 +0200

    Add project files.

---
+++ SyncthingStatus/ApiClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SyncthingStatus
+{
+    class ApiClient
+    {
+        private static HttpClient client;
+
+        internal static void Initialize()
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri("http://127.0.0.1:8384/");
+            client.DefaultRequestHeaders.Add("X-API-Key", Properties.Settings.Default.ApiKey);
+        }
+
+        internal static async Task<string> ping()
+        {
+            try
+            {
+                var result = await client.GetStringAsync("/rest/system/ping");
+                return result;
+            } catch(HttpRequestException e)
+            {
+                return "";
+            }
+        }
+    }
+}
---
+++ SyncthingStatus/TrayIconContainer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SyncthingStatus
+{
+    class TrayIconContainer
+    {
+        private NotifyIcon trayIcon;
+        private ContextMenuStrip trayMenu;
+        private SettingsForm settingsForm;
+
+        private ToolStripMenuItem trayMenuItemAbout;
+        private ToolStripSeparator trayMenuSeparator1;
+        private ToolStripMenuItem trayMenuItemOpen;
+        private ToolStripMenuItem trayMenuItemSettings;
+        private ToolStripMenuItem trayMenuItemExit;
+
+        public TrayIconContainer()
+        {
+            InitializeComponents();
+            yeet();
+        }
+
+        async void yeet()
+        {
+            Console.WriteLine("Current status is:" + await ApiClient.ping());    
+        }
+
+        private void InitializeComponents()
+        {
+            trayMenuItemAbout = new ToolStripMenuItem();
+            trayMenuItemAbout.Text = Util.GetAboutString();
+            trayMenuItemAbout.Enabled = false;
+
+            trayMenuSeparator1 = new ToolStripSeparator();
+
+            trayMenuItemOpen = new ToolStripMenuItem();
+            trayMenuItemOpen.Text = "Open in browser";
+            trayMenuItemOpen.Click += TrayMenuItemOpenClickHandler;
+
+            trayMenuItemSettings = new ToolStripMenuItem();
+            trayMenuItemSettings.Text = "Settings";
+            trayMenuItemSettings.Click += TrayMenuItemSettingsClickHandler;
+
+            trayMenuItemExit = new ToolStripMenuItem();
+            trayMenuItemExit.Text = "Exit";
+            trayMenuItemExit.Click += TrayMenuItemExitClickHandler;
+
+            trayMenu = new ContextMenuStrip();
+            trayMenu.ShowImageMargin = false;
+
+            trayMenu.Items.Add(trayMenuItemAbout);
+            trayMenu.Items.Add(trayMenuSeparator1);
+            trayMenu.Items.Add(trayMenuItemOpen);
+            trayMenu.Items.Add(trayMenuItemSettings);
+            trayMenu.Items.Add(trayMenuItemExit);
+
+            trayIcon = new NotifyIcon();
+            trayIcon.Icon = Properties.Resources.iconDefault;
+            trayIcon.Visible = true;
+            trayIcon.ContextMenuStrip = trayMenu;
+            trayIcon.DoubleClick += TrayMenuItemOpenClickHandler;
+
+            settingsForm = new SettingsForm();
+        }
+
+        private void TrayMenuItemOpenClickHandler(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("http://localhost:8384");
+        }
+
+        private void TrayMenuItemSettingsClickHandler(object sender, EventArgs e)
+        {
+            if (settingsForm.IsDisposed)
+            {
+                settingsForm = new SettingsForm();
+            }
+            settingsForm.Show();
+        }
+
+        private void TrayMenuItemExitClickHandler(object sender, EventArgs e)
+        {
+            // Tray icon sometimes just hangs around until user hovers over it, so
+            // explicitly hide it before actual exit.
+            trayIcon.Visible = false;
+            Application.Exit();
+        }
+    }
+}
